c54055ea5c3661f61008a8aa83d314c62a8d2580
ADM-ShobhitP <shobhitprakash8@gmail.com>
2025-04-08 14:27:20 +0530

    ngork

--- service_axios.js
+++ service_axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const service = axios.create({
-    baseURL: "http://10.1.15.162:8000",
+    baseURL: process.env.NODE_ENV=='development'?"http://10.1.15.162:8000":"https://7400-1-7-16-2.ngrok-free.app",
 });
 
 export default service;
