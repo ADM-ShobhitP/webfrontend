d4717d4828024912fdb240ca99c04630f1bac292
ADM-ShobhitP <shobhitprakash8@gmail.com>
2025-04-09 15:36:49 +0530

    ngrok

--- service_axios.js
+++ service_axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const service = axios.create({
-    baseURL: process.env.NODE_ENV == 'development' ? "http://10.1.15.162:8000" : "https://2c10-1-7-16-2.ngrok-free.app",
+    baseURL: process.env.NODE_ENV == 'development' ? "http://10.1.15.162:8000" : "https://7521-1-7-16-2.ngrok-free.app",
     headers: {
         "ngrok-skip-browser-warning": 234
     }
